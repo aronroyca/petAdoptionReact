b2d48e6f69d6bf2d0188b75b21cd484f55ae423e
Aron Kumar Aroy <113210093+aronroyca@users.noreply.github.com>
2023-02-10 12:12:44 -0800

    modularized results section

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
-import Pet from "./Pet.jsx"
 import useBreedList from './useBreedList.jsx';
-const ANIMALS = ['bird', 'cat', 'dog', 'rabbit', 'reptile']
+import Results from './Results';
+const ANIMALS = ['bird', 'cat', 'dog', 'rabbit', 'reptile'];
 
 const SearchParams = () => {
     const [location, setLocation] = useState("");
@@ -72,14 +72,7 @@ const SearchParams = () => {
                 </label>
                 <button>Submit</button>
             </form>
-            {
-                pets.map(pet => (
-                    <Pet name={pet.name}
-                        animal={pet.animal}
-                        breed={pet.breed}
-                        key={pet.id} />
-                ))
-            }
+            <Results pets={pets} />
         </div>
     )
 }
